7a00c5244bf11c811035166c002854cbe0d76d82
Aakash Kumar <aakashkumar051901@gmail.com>
2023-05-06 12:43:56 +0530

    Assignment solved attempt 1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,9 @@
 package com.driver;
 
+import java.util.function.IntPredicate;
+import java.util.regex.*;
+
+
 public class Email {
 
     private String emailId;
@@ -18,12 +22,28 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+
+
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if (oldPassword.equals(password)) {
+            if (newPassword.length() >= 8 && isValidPass(newPassword)) {
+                password = newPassword;
+            }
+        }
+    }
+
+    private boolean isValidPass(String newPassword) {
+        String regex = "^(?=.*[a-z])(?=." + "*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(newPassword);
+        if (m.matches())return true;
+        return false;
     }
 }
